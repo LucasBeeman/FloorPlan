59e7bbc29c05edb8278c3ecacd28fe329d13aef3
beeman_lucas <beeman_lucas@G-02-20>
2021-09-15 08:47:28 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ArchitectArithmetic
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double addedUp = rectangle(2500, 1500) + circle(375) + triangle(750, 500);
+            double completCost = addedUp * 180;
+            double costRounded = Math.Round(completCost, 2);
+            Console.WriteLine($"The floor will cost {costRounded} pesos.");
+            Console.ReadKey();
+        }
+
+        static double rectangle(double length, double width)
+        {
+            double area = length * width;
+            return area;
+        }
+
+        static double circle(double radius)
+        {
+            double area = Math.PI * Math.Pow(radius, 2);
+            return area;
+        }
+
+        static double triangle(double bottom, double height)
+        {
+            double area = .5 * bottom * height;
+            return area;
+        }
+    }
+}
